76066c73e786f575d2643265ca9fd1067303c9f6
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-04-10 01:13:28 +0530

    custom text for the onboarding pages with easily customizable font styles

--- app/src/main/java/com/example/notesapp/ui/components/CustomText.kt
+++ app/src/main/java/com/example/notesapp/ui/components/CustomText.kt
@@ -1,4 +1,29 @@
 package com.example.notesapp.ui.components
 
-class CustomText {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomText(
+    text: String,
+    textColor: Color,
+    fontFamily: FontFamily,
+    fontSize: Int,
+    align: TextAlign = TextAlign.Unspecified,
+    onClick: () -> Unit = {}
+) {
+    Text(
+        text = text,
+        style = TextStyle(
+            fontFamily = fontFamily,
+            fontSize = (fontSize).sp,
+            color = textColor,
+            textAlign = align
+        )
+    )
 }
