6bd7c38648ed89c680ec02c851f1466f1d1c2595
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-04-09 17:51:28 +0530

    added splash screen

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.notesapp.ui.screens.SplashScreen
 import com.example.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    SplashScreen(navController = rememberNavController(), isFirstTime = true)
                 }
             }
         }
---
+++ app/src/main/java/com/example/notesapp/ui/screens/SplashScreen.kt
@@ -0,0 +1,61 @@
+package com.example.notesapp.ui.screens
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.EaseInBounce
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.notesapp.R
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    isFirstTime: Boolean
+) {
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                1200,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            ),
+        )
+    }
+
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = modifier
+                .size((200 * scale.value).dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.notes_latest),
+            contentDescription = null,
+
+        )
+    }
+}
